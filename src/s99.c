47aad9d7b012146ead54f90105caefb56c888263
Sachin T <sachintu47@gmail.com>
2026-07-29 19:27:27 +0530

    Update s99.c

--- src/s99.c
+++ src/s99.c
@@ -264,3 +264,82 @@ int s99_prt_msg(DBG_Opts* opts, struct s99rb* __ptr32 svc99parms, int svc99rc)
 
 	return rc;
 }
+
+#ifndef IOSVC_ERR_NOERROR
+#define IOSVC_ERR_NOERROR                  0
+#define IOSVC_ERR_SVC99INIT_ALLOC_FAILURE  4
+#define IOSVC_ERR_SVC99_ALLOC_FAILURE      8
+#endif
+
+static const struct s99_rbx s99rbxtemplate = {"S99RBX",S99RBXVR,{0,1,0,0,0,0,0},0,0,0};
+
+int dsdd_alloc(struct s99_common_text_unit* dsn, struct s99_common_text_unit* dd, struct s99_common_text_unit* disp, const DBG_Opts* opts)
+{
+  struct s99rb* __ptr32 parms;
+  enum s99_verb verb = S99VRBAL;
+  struct s99_flag1 s99flag1 = {0};
+  struct s99_flag2 s99flag2 = {0};
+  size_t num_text_units = 3;
+  int rc;
+  struct s99_rbx s99rbx = s99rbxtemplate;
+
+  parms = s99_init(verb, s99flag1, s99flag2, &s99rbx, num_text_units, dsn, dd, disp);
+  if (!parms) {
+    return IOSVC_ERR_SVC99INIT_ALLOC_FAILURE;
+  }
+  rc = S99(parms);
+  if (rc) {
+#ifdef DEBUG
+    s99_fmt_dmp((DBG_Opts*)opts, parms);
+#endif
+    s99_prt_msg((DBG_Opts*)opts, parms, rc);
+    return IOSVC_ERR_SVC99_ALLOC_FAILURE;
+  }
+
+  struct s99_common_text_unit* ddout = (struct s99_common_text_unit*) parms->s99txtpp[1];
+  dd->s99tulng = ddout->s99tulng;
+  memcpy(dd->s99tupar, ddout->s99tupar, dd->s99tulng);
+
+  s99_free(parms);
+  return IOSVC_ERR_NOERROR;
+}
+
+int ddfree(struct s99_common_text_unit* dd)
+{
+  struct s99rb* __ptr32 parms;
+  enum s99_verb verb = S99VRBUN;
+  struct s99_flag1 s99flag1 = {0};
+  struct s99_flag2 s99flag2 = {0};
+  size_t num_text_units = 1;
+  int rc;
+  struct s99_rbx s99rbx = s99rbxtemplate;
+
+  parms = s99_init(verb, s99flag1, s99flag2, &s99rbx, num_text_units, dd);
+  if (!parms) {
+    return 16;
+  }
+  rc = S99(parms);
+  if (rc) {
+#ifdef DEBUG
+    s99_fmt_dmp(NULL, parms);
+#endif
+    s99_prt_msg(NULL, parms, rc);
+    return rc;
+  }
+
+  s99_free(parms);
+  return 0;
+}
+
+int init_dsnam_text_unit(const char* dsname, struct s99_common_text_unit* dsn, const DBG_Opts* opts)
+{
+  size_t dsname_len = (dsname == NULL) ? 0 : strlen(dsname);
+  if (dsname == NULL || dsname_len == 0 || dsname_len > DS_MAX) {
+    errmsg(opts, "Dataset Name <%.*s> is invalid\n", dsname_len, dsname);
+    return 8;
+  }
+
+  dsn->s99tulng = dsname_len;
+  memcpy(dsn->s99tupar, dsname, dsname_len);
+  return 0;
+}
